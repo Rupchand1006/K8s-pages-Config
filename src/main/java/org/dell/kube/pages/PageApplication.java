86e0bfdf7cddacfdb7a2979d5fc5e582fd74d2be
Rupchand_Dewangan <Rupchand_Dewangan@dell.com>
2020-10-18 17:13:17 +0530

    first commit

---
+++ src/main/java/org/dell/kube/pages/PageApplication.java
@@ -0,0 +1,18 @@
+package org.dell.kube.pages;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
+
+@SpringBootApplication
+public class PageApplication {
+
+	
+	public static void main(String[] args) {
+		SpringApplication.run(PageApplication.class, args);
+	}
+
+
+}
